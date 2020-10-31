3cf18b94232b253e88afe17b1c73357f07b92d97
Максим Шаров <sm710mh@gmail.com>
2020-10-31 17:50:46 +0300

    feat: solved tasks Carbon dating, Count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  var count = 0;
+  for(var i = 0; i < matrix.length; i++) {
+    for(var k = 0; k < matrix[i].length; k++) {
+      matrix[i][k] == '^^' ? count += 1 : false;
+    }
+  }
+  return count;
 };
