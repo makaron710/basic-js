be65bc9b8f92e25b177caafb78768b30982945d4
Максим Шаров <sm710mh@gmail.com>
2020-12-14 19:14:14 +0300

    feat: the task of 'vigenere-cipher' solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(straight) {
+    this.straight = straight;
+  }
+
+  encrypt(string, key) {
+    var keyString = key.repeat(Math.ceil(string.length / key.length)).toUpperCase();
+    var newString = string.toUpperCase().split("");
+    for(let i = 0, j = 0; i < string.length; i++, j++) {
+      if(/[A-Z]/.test(newString[i])) {
+        newString[i] = String.fromCharCode(((((newString[i].charCodeAt()) + (keyString[j].charCodeAt())) - 130) % 26) + 65);
+      } else {
+        j--;
+      }
+    }
+    if(this.straight != false) {
+      return newString.join("");
+    } else {
+      return newString.reverse().join("");
+    }
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(string, key) {
+    var keyString = key.repeat(Math.ceil(string.length / key.length)).toUpperCase();
+    var newString = string.toUpperCase().split("");
+    for(let i = 0, j = 0; i < string.length; i++, j++) {
+      if(/[A-Z]/.test(newString[i])) {
+        newString[i] = String.fromCharCode(((((newString[i].charCodeAt() - 65) - (keyString[j].charCodeAt() - 65)) + 26) % 26) + 65)
+      } else {
+        j--;
+      }
+    }
+    if(this.straight != false) {
+      return newString.join("");
+    } else {
+      return newString.reverse().join("");
+    }
   }
 }
 
